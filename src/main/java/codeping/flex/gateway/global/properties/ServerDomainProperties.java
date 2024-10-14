0ded7b8643210d0ef4115dd9a84f58bf53661d67
joowojr <joowojr@gmail.com>
2024-10-14 19:59:01 +0900

    FLEX-108 :sparkles: Feature: serverDomainProperties 추가
    <footer>
    - 관련 : #11

---
+++ src/main/java/codeping/flex/gateway/global/properties/ServerDomainProperties.java
@@ -0,0 +1,14 @@
+package codeping.flex.gateway.global.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "server.domain")
+public class ServerDomainProperties {
+    private final String local;
+    private final String service;
+}
