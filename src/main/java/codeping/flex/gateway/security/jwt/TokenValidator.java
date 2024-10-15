49a52dea7efccf6fc2c4638e0ce35bf236ebf31c
joowojr <joowojr@gmail.com>
2024-10-15 10:02:24 +0900

    FLEX-108 :recycle: Refactor: 토큰 추출 및 유효성 검증 로직 TokenValidator로 책임 이동
    <footer>
    - 관련 : #11

--- src/main/java/codeping/flex/gateway/security/jwt/TokenValidator.java
+++ src/main/java/codeping/flex/gateway/security/jwt/TokenValidator.java
@@ -8,27 +8,6 @@ import java.time.LocalDateTime;
 import static codeping.flex.gateway.security.jwt.AuthConstants.TOKEN_PREFIX;
 
 public interface TokenValidator {
-    /**
-     * 헤더로부터 토큰을 추출하고 유효선을 검증하는 메서드
-     *
-     * @param header : Authorization 헤더
-     * @return 값이 있다면 토큰, 없다면 빈 문자열
-     */
-    default String resolveToken(String header) {
-        if (StringUtils.hasText(header) && header.startsWith(AuthConstants.TOKEN_PREFIX.getValue())) {
-            return header.substring(TOKEN_PREFIX.getValue().length());
-        }
-        return "";
-    }
-
-    /**
-     * 토큰을 파싱하여 JwtClaims 객체로 변환하는 메서드
-     *
-     * @param token : 토큰
-     * @return 사용자 정보를 담은 {@link JwtClaims} 객체
-     */
-    JwtClaims parseJwtClaimsFromToken(String token);
-
 
     /**
      * 토큰이 만료되었는지 확인하는 메서드
