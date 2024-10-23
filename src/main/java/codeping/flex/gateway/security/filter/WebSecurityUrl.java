e36258963cdfb338ea71dfb405db47045c799ee6
joowojr <joowojr@gmail.com>
2024-10-23 21:47:50 +0900

    :sparkles: Feature: 메인 랜딩페이지 익명 유저 접근 허용

--- src/main/java/codeping/flex/gateway/security/filter/WebSecurityUrl.java
+++ src/main/java/codeping/flex/gateway/security/filter/WebSecurityUrl.java
@@ -7,7 +7,7 @@ public class WebSecurityUrl {
     }
 
     protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico"};
-    protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/auth/login/**", "/api/auth/signup/**"};
+    protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/auth/login/**", "/api/auth/signup/**", "/api/blogs/landing"};
     protected static final String[] EXCLUDE_TOKEN_VALIDATION = {"/api/auth/reissue"};
     protected static final String[] SWAGGER_ENDPOINTS = {
             "/api/v3/api-docs/**",
